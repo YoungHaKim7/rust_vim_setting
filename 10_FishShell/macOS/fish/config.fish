0cc90434fb7fec1cf070ba0816968f3a0f7556cb
GYoung <ytok1108@aol.com>
2024-03-13 21:30:12 +0900

    fish ++ Add function

--- 10_FishShell/macOS/fish/config.fish
+++ 10_FishShell/macOS/fish/config.fish
@@ -9,6 +9,25 @@ fish_add_path "$HOME/utilities/zls/zig-out/bin"
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
+
+# add Path
+function addpaths
+    contains -- $argv $fish_user_paths
+       or set -U fish_user_paths $fish_user_paths $argv
+    echo "Updated PATH: $PATH"
+end
+
+# Remove path
+function removepath
+    if set -l index (contains -i $argv[1] $PATH)
+        set --erase --universal fish_user_paths[$index]
+        echo "Updated PATH: $PATH"
+    else
+        echo "$argv[1] not found in PATH: $PATH"
+    end
+
+end
+
 end
 
 # Enable Starship prompt
