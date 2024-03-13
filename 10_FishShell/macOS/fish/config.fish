0fca651eef3af4305b6d50e50fde072e9ebc909f
GYoung <ytok1108@aol.com>
2024-03-13 21:19:10 +0900

    fish shell  macOS

--- 10_FishShell/macOS/fish/config.fish
+++ 10_FishShell/macOS/fish/config.fish
@@ -1,10 +1,15 @@
 # Add HomeBrew's bin directory to path so you can use HomeBrew's binaries like `starship`
 # Fish uses `fish_add_path` instead of `export PATH` modify $PATH.
 fish_add_path "/opt/homebrew/bin/"
+fish_add_path "$HOME/.local/bin"
+fish_add_path "$HOME/utilities/nvim-macos"
+fish_add_path "$HOME/utilities/zig_0_12"
+fish_add_path "$HOME/utilities/zls/zig-out/bin"
 
-# Enable Starship prompt
-starship init fish | source
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# Enable Starship prompt
+starship init fish | source
