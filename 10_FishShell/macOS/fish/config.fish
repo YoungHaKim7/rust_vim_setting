7f42b7ce3e73cc52abd7cfa843e39b826b905211
GYoung <ytok1108@aol.com>
2025-02-27 20:27:06 +0900

    macOS PATH250227

--- 10_FishShell/macOS/fish/config.fish
+++ 10_FishShell/macOS/fish/config.fish
@@ -1,7 +1,5 @@
 # Add HomeBrew's bin directory to path so you can use HomeBrew's binaries like `starship`
 # Fish uses `fish_add_path` instead of `export PATH` modify $PATH.
-fish_add_path /opt/homebrew/bin/
-fish_add_path /opt/homebrew/include/
 fish_add_path "$HOME/.local/bin"
 fish_add_path "$HOME/utilities/nvim-macos"
 fish_add_path "$HOME/utilities/zig"
@@ -13,6 +11,20 @@ fish_add_path "$HOME/.modular/bin"
 fish_add_path "$HOME/.modular/pkg/packages.modular.com_mojo/bin"
 fish_add_path "$HOME/Library/Application Support/Code/User/globalStorage/fwcd.kotlin/langServerInstall/server/bin"
 fish_add_path "$HOME/.surrealdb"
+fish_add_path "$HOME/utilities/c3-lsp/server/bin/release"
+
+fish_add_path /opt/homebrew/bin/
+fish_add_path /opt/homebrew/opt/llvm/bin
+fish_add_path "$HOME/utilities/c3_compiler"
+fish_add_path /opt/homebrew/opt/llvm@17/bin
+fish_add_path /opt/homebrew/include/
+fish_add_path /usr/local/include/opencv4
+fish_add_path /opt/homebrew/opt/libpq/bin
+
+fish_add_path /System/Volumes/Data/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks/OpenGL.framework/Versions/A/Headers
+fish_add_path -U /Users/gy-gyoung/utilities/glfw/include
+fish_add_path /opt/homebrew/lib
+
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
@@ -43,10 +55,30 @@ if status is-interactive
 
     # macOS SDL 2 , 3 Path
     set -gx DYLD_FRAMEWORK_PATH /Library/Frameworks
-    set -gx LIBRARY_PATH /opt/homebrew/lib
+
+    set -gx LIBRARY_PATH /opt/homebrew/lib /opt/homebrew/include
+
+    # OpenCV
+    set -gx PKG_CONFIG_PATH /opt/homebrew/bin
 
     # sdk man(java, kotlin)
     # set -gx
+
+    # LLVM
+    set -gx LDFLAGS -L/opt/homebrew/opt/llvm/lib
+    set -gx CPPFLAGS -I/opt/homebrew/opt/llvm/include
+
+    # LLVM 17(c3c)
+    set -gx LDFLAGS "-L/opt/homebrew/opt/llvm@17/lib"
+    set -gx CPPFLAGS "-I/opt/homebrew/opt/llvm@17/include"
+
+    # postgres PATH
+    # For compilers to find libpq you may need to set:
+    set -gx LDFLAGS -L/opt/homebrew/opt/libpq/lib
+    set -gx CPPFLAGS -I/opt/homebrew/opt/libpq/include
+
+    #For pkg-config to find libpq you may need to set:
+    set -gx PKG_CONFIG_PATH /opt/homebrew/opt/libpq/lib/pkgconfig
 
 end
 
